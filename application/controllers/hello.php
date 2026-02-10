663919c2cbc095ff91d84376abd0b0fd726c14dc
cindysetio828-pixel <cindysetio828@gmail.com>
2026-02-10 15:47:06 +0700

    Update hello.php

--- application/controllers/hello.php
+++ application/controllers/hello.php
@@ -1,5 +1,15 @@
 <?php
 
-class hello extends C1_Controller {
-  
+class hello extends CI_Controller {
+
+  public function index()
+{
+  $data['nama'] = "Queen Cindi";
+  $this->load->view('hello_view',$data);
+}
+public function namasaya()
+{
+  $data['nama'] = "Queen";
+  $this->load->view('hello_view',$data);
+}
 }
