3d7166a403a683dde1fd3d33add8aec1a3e32851
cindysetio828-pixel <cindysetio828@gmail.com>
2026-03-03 15:49:36 +0700

    pertemuan 3

---
+++ application/views/templates/footer.php
@@ -0,0 +1,9 @@
+</div>
+</div>
+
+<script src="<?= base_url('assets/vendor/iquery.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/bootstrap/js/boots.bundle.min.js'); ?>"></script>
+<script src="<?= base_url('assets/vendor/js/sb-admin-2.min.js'); ?>"></script>
+
+</body>
+</html>
