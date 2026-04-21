b194c3bb0d630c5ba7210a60c58b035347198224
cindysetio828-pixel <cindysetio828@gmail.com>
2026-04-21 15:35:58 +0700

    pertemuan 8

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,35 @@
+<h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
+
+<div class="row">
+    <!-- kategori -->
+     <div class="col-xl-4 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card body">
+                <h5> Total Kategori</h5>
+                <h3><?= $total_kategori; ?></h3>
+            </div>
+        </div>
+    </div>
+    <!-- Buku -->
+    <div class="col-xl-4 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+            <div class="card body">
+                <h5> Total Buku</h5>
+                <h3><?= $total_buku; ?></h3>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+    <div class="col-xl-6 col-lg-12">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            </div>
+            <div class="card body">
+                <div class="chart-area">
+                    <canvas id="chartDashboard" style="max-height: 200px;"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
